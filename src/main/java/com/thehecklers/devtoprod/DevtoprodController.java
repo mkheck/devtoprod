ddc401954daaf200113a18268c017b049fae1156
Mark Heckler <mark.heckler@gmail.com>
2025-04-11 10:46:00 -0500

    For Nate

--- src/main/java/com/thehecklers/devtoprod/DevtoprodController.java
+++ src/main/java/com/thehecklers/devtoprod/DevtoprodController.java
@@ -1,6 +1,7 @@
 package com.thehecklers.devtoprod;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class DevtoprodController {
     @GetMapping
     public String liveness() {
         return "Dev to Prod demo app is ALIVE!!!!";
+    }
+    
+    @GetMapping("/echo")
+    public String echo(@RequestParam(defaultValue = "Hello there!") String message) {
+        return "Echo: " + message;
     }
 }
